dec921aafae99e96a40080c51c65487b51a29253
Giuliano <giulianoiantomasi@outlook.com>
2023-04-21 19:54:37 -0400

    Boss Animations and Player Chase script beta
    Animations faulty, might need to ask teacher for another way to do this (for player animation we have to take into account BOTH x & y axis when switching between animations!!)

--- Assets/Script/ChasePlayer.cs
+++ Assets/Script/ChasePlayer.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class ChasePlayer : MonoBehaviour
 {
+
+    Animator animator;
+
+
     public GameObject target;
     public float speed;
     private float distance;
@@ -12,20 +16,59 @@ public class ChasePlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        float val_x = Input.GetAxis("Horizontal");
+        float val_y = Input.GetAxis("Vertical");
+
+        if (val_y < 0 && val_x > 3)//then set direction to 2
+        {
+            animator.SetInteger("direction", (int)MoveDirection.RIGHT);
+        }
+
+        else if (val_y > 0) //then set direction to 1
+        {
+            animator.SetInteger("direction", (int)MoveDirection.UP);
+        }
+
+
+        else if (val_x < 0) //then set direction to 3
+        {
+            animator.SetInteger("direction", (int)MoveDirection.LEFT);
+        }
+
+
+        else if(val_x > 0) //then set direction to 4
+        {
+            animator.SetInteger("direction", (int)MoveDirection.RIGHT);
+        }
+
+       
         distance = Vector2.Distance(transform.position, target.transform.position);
-        Vector2 direction = target.transform.position - transform.position; 
+        Vector2 direction = target.transform.position - transform.position;
         direction.Normalize();
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
         transform.position = Vector2.MoveTowards(this.transform.position, target.transform.position, speed * Time.deltaTime);
-        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        //transform.rotation = Quaternion.Euler(Vector3.forward * angle);
 
 
     }
 }
+
+
+public enum MoveDirection
+{
+    NONE = 0,
+    UP = 1,
+    DOWN = 2,
+    LEFT = 3,
+    RIGHT = 4
+}
+
+
