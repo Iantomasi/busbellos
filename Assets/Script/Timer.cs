1f3468b29f084ad4d9099bbc6bdfb75a72aa9864
Timbro <alexei.timbro@gmail.com>
2023-05-02 22:32:18 -0400

    j

--- Assets/Script/Timer.cs
+++ Assets/Script/Timer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameOver : MonoBehaviour
+public class Timer : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
